ccfd007e59e33d0d5e83506e0befa866ed572cc1
Pratik786610 <152353594+Pratik786610@users.noreply.github.com>
2024-02-08 17:26:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,15 +1,15 @@
 # ©️biisal jai shree krishna 😎
 from os import environ
 
-API_ID = environ.get('API_ID','')
-API_HASH = environ.get('API_HASH','')
-BOT_TOKEN = environ.get('BOT_TOKEN','')
+API_ID = environ.get('API_ID','29712643')
+API_HASH = environ.get('API_HASH','159ec16a16e76a7f5e51d3c7e157e386')
+BOT_TOKEN = environ.get('BOT_TOKEN','6957887097:AAGBF-s01pLb7NXUOF3pWHGElQWMnRh5ti4')
 BOT_NAME = environ.get('BOT_NAME','BISAL CHAT BOT')
-ADMIN = int(environ.get('ADMIN',''))
-CHAT_GROUP = int(environ.get('CHAT_GROUP','-1001812797837'))
+ADMIN = int(environ.get('ADMIN','1691590963'))
+CHAT_GROUP = int(environ.get('CHAT_GROUP','-1002020168110'))
 ADMIN_NAME = environ.get('ADMIN_NAME','Bisal')
-LOG_CHANNEL = environ.get('LOG_CHANNEL','')
-MONGO_URL = environ.get('MONGO_URL','')
+LOG_CHANNEL = environ.get('LOG_CHANNEL','-1002050735539')
+MONGO_URL = environ.get('MONGO_URL','mongodb+srv://zalivett444:eyO6VsxQAh4JhEqU@cluster0.j4zzhw6.mongodb.net/?retryWrites=true&w=majority')
 STICKERS_IDS = (
     "CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME"
 ).split()
